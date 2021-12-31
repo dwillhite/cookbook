9d8ae3bb6a1784866544bb4081bc20abd1d3dad4
Dave Willhite <davidmwillhite@gmail.com>
2021-12-31 10:55:16 -0700

    intitial commit

---
+++ src/components/Recipe.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+</script>
+
+<template>
+<div>
+  <h1>{{ msg }}</h1>
+<p>empire</p>
+  <input type="text">
+</div>
+
+</template>
+
+<style scoped>
+
+</style>
