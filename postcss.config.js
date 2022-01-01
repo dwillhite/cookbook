eebaa2b1b98dd44172dbf415e18a21038f3116ff
Dave Willhite <davidmwillhite@gmail.com>
2022-01-01 11:12:23 -0700

    install tailwind

---
+++ postcss.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  plugins: [
+    // tailwindcss: {},
+    // autoprefixer: {},
+    require('postcss-import'),
+    require('tailwindcss'),
+    require('autoprefixer'),
+  ],
+}
